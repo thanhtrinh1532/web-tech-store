9f0d671ade1a499286338670dbe56ee9d55494d5
ThiênLang <isanagiyashiro@gmail.com>
2025-06-05 15:20:58 +0700

    Hoàn thành F01: Khởi tạo dự án React và cấu hình định tuyến

---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+     const Home = () => {
+       return (
+         <div>
+           <h1>Tech Store - Home Page</h1>
+           <p>Chào mừng đến với Tech Store!</p>
+         </div>
+       );
+     };
+
+     export default Home;
